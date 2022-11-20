5510c25d3afb29394fb0394bc3c4548293803a48
helenbruker <115081449+helenbruker@users.noreply.github.com>
2022-11-20 17:21:11 +0100

    task 2: keys

---
+++ notebooks/keys.py
@@ -0,0 +1,19 @@
+# PYTHON FILE WITH TWITTER KEYS
+
+"""
+This file contains your Twitter keys
+
+    Replace the text with your keys
+    Keep the quotation marks
+
+"""
+
+consumer_key='qY2Mam3Ycuz96dsgctbuAcPuV'
+
+consumer_secret='3j2ooIFRdi48Rmw30bMydgRupE9twj5kHroN0SL6ukJzjqX1BF'
+
+access_token='1582676468622147585-Dn4GWLOh7FI3bskb1UJtAtkopqe5Pc'
+
+access_token_secret='KcJUN47pgSRUnEEyrMjoVEmmtnWLyXOPCYsOsYXOxvzfM'
+
+bearer_token='AAAAAAAAAAAAAAAAAAAAAFtgiQEAAAAAEKdKYU%2BYgc0vwQHNY4I%2BL7xjcOI%3D6pQp7fIXl2o78OF2Omx0SUAGRCVpJMWWtqpnDqgBgc7AjyzuaM'
