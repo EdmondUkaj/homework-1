9ff0eeead77b8720cf00481d4b05393a98742479
EdmondUkaj <115081672+EdmondUkaj@users.noreply.github.com>
2022-12-16 00:45:40 +0100

    sidebar

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,3 +7,11 @@ home_path = str(Path.home())
 
 df = pd.read_csv('fifa_countries_audience.csv')
 
+st.title("Stramlit")
+st.image("hdm-logo.jpg")
+st.header("Group K's Dashboard")
+
+st.sidebar.header("Congrats, you've opened the sidebar!")
+satisfation = st.sidebar.slider("How satisfied are you with this dashboard",0 , 10, 1)
+st.sidebar.write("I rate this dashboard", satisfation, "/10")
+
