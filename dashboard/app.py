d729f7429d93497b821dc94c43f9ed84d60b7085
EdmondUkaj <115081672+EdmondUkaj@users.noreply.github.com>
2022-12-16 17:32:13 +0100

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,7 +7,7 @@ home_path = str(Path.home())
 
 df = pd.read_csv('/Users/edmondukaj/big_data/homework-1/data/external/fifa_countries_audience.csv')
 
-st.title("Stramlit")
+st.title("Streamlit")
 st.image("hdm-logo.jpg")
 st.header("Group K's Dashboard")
 
@@ -16,3 +16,31 @@ satisfation = st.sidebar.slider("How satisfied are you with this dashboard",0 , 
 st.sidebar.write("I rate this dashboard", satisfation, "/10")
 
 st.dataframe(df)
+
+st.write("First chart")
+
+c = alt.Chart(df).mark_bar().encode(
+        x=('confederation'),
+        y=alt.Y('count(confederation)')
+)
+st.altair_chart(c, use_container_width=True)
+
+st.write("Second Chart")
+
+c = alt.Chart(df).mark_bar().encode(
+    x=alt.X('confederation',
+            sort='-y',
+            axis=alt.Axis(title="Confederations", 
+                          titleAnchor="start", 
+                          labelAngle=0)),
+    y=alt.Y('count(confederation)', 
+            axis=alt.Axis(title = "Count", 
+                          titleAnchor="end")),
+    color= alt.Color('count(confederation)', 
+                     legend=alt.Legend(title="Count"))
+).properties(
+    title='FIFA Confederations',
+    width=350,
+    height=150
+)
+st.altair_chart(c, use_container_width=True)
