2e086b804cbd342aaa4ec9ac5ba44b94de134f43
EdmondUkaj <115081672+EdmondUkaj@users.noreply.github.com>
2022-12-18 22:48:14 +0100

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -76,12 +76,20 @@ st.write("fifth chart")
 
 source = pd.DataFrame({"category": ['confederation'], "value": ['confederation_count']})
 
-c = alt.Chart(df).mark_arc().encode(
-    theta=alt.Theta(field="value", type="quantitative"),
-    color=alt.Color(field="category", type="nominal"),
+source = pd.DataFrame(df.confederation.value_counts())
+source = source.reset_index()
+source.rename(columns={"index": "category", "confederation": "value"}, inplace=True)
+source
+
+chart = alt.Chart(source).encode(
+    theta=alt.Theta("value:Q", stack=True), 
+    color=alt.Color("category:N", legend=None)
+).properties(
+    title='FIFA Confederations',
 )
-pie = base.mark_arc(outerRadius=120)
-text = base.mark_text(radius=140, size=20).encode(text="category:N")
+
+pie = chart.mark_arc(outerRadius=100)
+text = chart.mark_text(radius=130, size=12).encode(text="category:N")
 
 pie + text
 
